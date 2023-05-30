de9ea55c969a2a31b79ab7adb865fc59071e528c
polrmn <polrmn@gmail.com>
2023-05-30 15:33:37 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edugain Overseas: learning platform
+          Edugain Overseas: Learning platform
         </p>
         <a
           className="App-link"
