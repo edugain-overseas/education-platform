94732e759a9a7f84022a8c666000a307cf901d03
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2023-06-12 16:34:48 +0300

    fix base URL

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "https://f941-194-44-219-51.ngrok-free.app/api/v1/",
+  baseURL: "https://e54a-194-44-219-51.ngrok-free.app/api/v1",
   headers: {
     "ngrok-skip-browser-warning": "1",
     "Content-Type": "application/x-wwww-form-urlencoded",
