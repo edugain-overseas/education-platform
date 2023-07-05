ef1fd338188c909cd94cda2d0b40d45e4bc6ba39
polrmn <polrmn@gmail.com>
2023-07-05 09:26:23 +0300

    add/chatFeed avatars for user

--- src/components/Chat/MessageFromChat/MessageFromChat.jsx
+++ src/components/Chat/MessageFromChat/MessageFromChat.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as SendFeedbackIcon } from "../../../images/icons/feedba
 import styles from "./MessageFromChat.module.scss";
 
 import user1Avatar from "../../../images/logo192.png";
+import { serverName } from "../../../constants/server";
 
 
 export function MessageFromChat({ message, type }) {
@@ -17,7 +18,11 @@ export function MessageFromChat({ message, type }) {
     >
       <div className={styles.avatarWrapper}>
         <img
-          src={user1Avatar}
+          src={
+            message.userData.ImagePath
+              ? `${serverName}${message.userData.ImagePath}`
+              : user1Avatar
+          }
           alt="avatar"
           className={styles.avatar}
         />
@@ -35,7 +40,10 @@ export function MessageFromChat({ message, type }) {
           {message.userData.Name} {message.message_datetime.slice(-8, -3)}
         </p>
         <div className={styles.contentWrapper}>
-          <div className={styles.content} dangerouslySetInnerHTML={{ __html: message.message_text }}></div>
+          <div
+            className={styles.content}
+            dangerouslySetInnerHTML={{ __html: message.message_text }}
+          ></div>
           <button className={styles.feedbackButton}>
             <span className={styles.feedbackButtonTitle}>Feedback</span>
             <SendFeedbackIcon />
